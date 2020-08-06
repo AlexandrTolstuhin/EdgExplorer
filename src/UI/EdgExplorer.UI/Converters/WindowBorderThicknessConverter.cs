08a1abb39fc6589ea3829721a6d7a59cf48ff16a
Alexandr Tolstuchin <alex.tolstuchin@gmail.com>
2020-08-06 13:11:41 +0500

    Add status bar styles

---
+++ src/UI/EdgExplorer.UI/Converters/WindowBorderThicknessConverter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Markup;
+
+namespace EdgExplorer.UI
+{
+    internal class WindowBorderThicknessConverter : MarkupExtension, IValueConverter
+    {
+        public override object ProvideValue(IServiceProvider serviceProvider) => this;
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            double.TryParse(parameter?.ToString(), out var length);
+
+            return value switch
+            {
+                WindowState state when state == WindowState.Maximized => new Thickness(0),
+                _ => new Thickness(length)
+            };
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+            => throw new NotImplementedException();
+    }
+}
