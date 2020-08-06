9361e500f097ef6bb8e7034894a04749a694ff88
Alexandr Tolstuchin <alex.tolstuchin@gmail.com>
2020-08-06 11:21:44 +0500

    Add WindowChrome

---
+++ src/UI/EdgExplorer.UI/Converters/WindowTitleHeightConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+using System.Windows.Markup;
+
+namespace EdgExplorer.UI
+{
+    internal class WindowTitleHeightConverter : MarkupExtension, IValueConverter
+    {
+        public override object ProvideValue(IServiceProvider serviceProvider) => this;
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+            => value switch
+            {
+                WindowState state when state == WindowState.Maximized => 32,
+                _ => 40
+            };
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+            => throw new NotImplementedException();
+    }
+}
